c8bfaf430d7cfdd1e334480a0d0637d8c15a8037
Jodel Labios <jodellabios866@gmail.com>
2026-05-21 12:37:59 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import taskReducer from "./taskSlice";
 
-// This is the central store that holds all app data
 const store = configureStore({
   reducer: {
     tasks: taskReducer,
